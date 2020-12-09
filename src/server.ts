0fcf67116bc1cd6b5e568bd229aeb65589e39c09
Vinicius Souza Feitosa <vinicius1997feitosa@gmail.com>
2020-12-08 22:25:55 -0300

    feat: criação da pasta routes e organização

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import router from './routes'
 
 const app = express();
+
+app.use(express.json())
+app.use(router)
 
 app.get('/', (req, res) => res.json({ message: 'ok' }));
 
 app.listen(3333, () => console.log('Subiu'));
+
+export default app
