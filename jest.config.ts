fb5158ef5fc0bd12626ecbc7c33fbc1e15b1d86c
Felipe <felipe1180@hotmail.com>
2020-12-09 23:57:35 -0300

    test: ignore pattern

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 export default {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  modulePathIgnorePatterns: ['dist', 'node_modules', 'coverage'],
+  modulePathIgnorePatterns: [
+    'dist',
+    'node_modules',
+    'coverage',
+    '<rootDir>/apps/*/*.entity.ts',
+  ],
   testMatch: ['**/?(*.)+(spec|test).(js|ts|tsx)'],
 };
